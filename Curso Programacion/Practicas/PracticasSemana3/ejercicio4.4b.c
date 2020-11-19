e2d803234c92c14ba71de2762cb62cd7e7496068
felipepresas <felipe.presas@gmail.com>
2020-11-20 00:33:50 +0100

    20112020033

--- Curso Programacion/Practicas/PracticasSemana3/ejercicio4.4b.c
+++ Curso Programacion/Practicas/PracticasSemana3/ejercicio4.4b.c
@@ -1,31 +1,34 @@
 /*B) ¿Sería posible en el switch-case pedir el nombre de la estación y devolver su letra inicial?*/
 
-#include<stdio.h>
-int main (){
-    char estacion,minuscula;
+#include <stdio.h>
+int main()
+{
+    char estacion, minuscula;
 
     printf("Indique la inicial de la estacion: ");
-    scanf("%c",&estacion);
-    minuscula=tolower(estacion);
-    
-    switch (minuscula){
-        case 'p':
-            printf("\nEs primavera\n");
+    scanf("%c", &estacion);
+    minuscula = tolower(estacion);
+
+    switch (minuscula)
+    {
+    case 'p':
+        printf("\nEs primavera\n");
         break;
-        case 'v':
-            printf("\nEs verano\n");
+    case 'v':
+        printf("\nEs verano\n");
         break;
-        case 'o':
-            printf("\nEs oto;o\n");
+    case 'o':
+        printf("\nEs oto;o\n");
         break;
-        case 'i':
-            printf("\nEs invierno\n");
+    case 'i':
+        printf("\nEs invierno\n");
         break;
-        default: printf("\nNo se encuentra esa estacion");
+    default:
+        printf("\nNo se encuentra esa estacion");
         break;
     }
     toupper(minuscula);
     printf("minuscula");
-    
+
     system("pause");
 }
