c20089907fd41210aaba71427416f6e9f3fb99a6
unknown <felipe.presas@gmail.com>
2020-11-20 12:29:26 +0100

    112234

---
+++ Curso Programacion/Practicas/PracticasSemana3/ejercicio5.1.c
@@ -0,0 +1,8 @@
+/* Si ese nombre es "mi nombre"(el de cada uno) nos 
+devolvera un mensaje que diga "X, has introducido tu nombre". 
+Si no lo es, dirá: "Tu amigo se llama X" */
+
+#include<stdio.h>
+int main (){
+    //declarar 
+}
