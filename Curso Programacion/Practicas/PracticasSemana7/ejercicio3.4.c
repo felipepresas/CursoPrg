8dbc782e49420e3daf634390be3aced171f0dd15
felipepresas <felipe.presas@gmail.com>
2020-12-16 13:46:06 +0100

    161220201346

--- Curso Programacion/Practicas/PracticasSemana7/ejercicio3.4.c
+++ Curso Programacion/Practicas/PracticasSemana7/ejercicio3.4.c
@@ -3,7 +3,7 @@ Pej: el factorial de 5 es: 5x4x3x2x1 */
 #include <stdio.h>
 int main()
 {
-    int i, j, num1, factorial = 0;
+    int i, num1, factorial = 0;
     printf("Introduzca un numero:");
     scanf("%d", &num1);
     for (factorial += num1, i = num1 - 1; i > 0; i--)
---
+++ Curso Programacion/Practicas/PracticasSemana7/ejercicio3.5.c
@@ -0,0 +1,28 @@
+/* Escribir un programa que permita al usuario 
+ingresar 6 números enteros, que pueden ser 
+positivos o negativos. Al finalizar, mostrar 
+la sumatoria de los números negativos y el 
+promedio de los positivos. */
+
+#include <stdio.h>
+int main (){
+    int i,num[6],sumNegativos=0,cont=0;
+    float sumPositivos=0;
+    for ( i = 0; i < 6; i++)
+    {
+          printf("Indique 6 numeros enteros: ");
+          scanf("%d",&num[i]);
+          if (num[i]<0)
+          {
+              sumNegativos+=num[i];
+          }
+          else
+          {
+              sumPositivos+=num[i];
+              cont++;
+          }
+    }
+    printf("Sumatoria de numeros negativos es: %d \n",sumNegativos);
+    printf("Promedio de los positivos es: %.2f \n",sumPositivos/cont*1.0);
+    system("pause");
+}
