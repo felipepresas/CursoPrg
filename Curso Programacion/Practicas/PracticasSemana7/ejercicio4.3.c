15eec95fc89a5081fd4105e3effb55040f0ef22f
felipepresas <felipe.presas@gmail.com>
2020-12-17 13:57:23 +0100

    171220201357

---
+++ Curso Programacion/Practicas/PracticasSemana7/ejercicio4.3.c
@@ -0,0 +1,32 @@
+/* Vamos a crear un programa en C que
+1) Pida la cantidad de numeros que queremos introducir
+2) Me pida introducir todos esos numeros y almacene
+3) Si hemos introducido el numero 0 en alguna posicion, cambiarlo 
+por un -1 e indicar que posicion ha sido modificada y
+ cuantas veces se ha encontrado el valor -1 */
+#include <stdio.h>
+int main()
+{
+    int i,j, cant,contZero=0;
+    printf("indique la cantidad: ");
+    scanf("%d",&cant);
+    int cadenaNumeros[cant];
+
+    for ( j = 0; j < cant; j++)
+    {
+        printf("Indique el %d numero : ",j+1);
+        scanf("%d",&cadenaNumeros[j]);
+    }
+    
+    for ( i = 0; i < cant; i++)
+    {
+        if (cadenaNumeros[i]==0)
+        {   
+            cadenaNumeros[i]=-1;
+            contZero++;
+        }
+        printf("Nuemros son:%d\n",cadenaNumeros[i]);
+    }
+    printf("Cantidad de ceros es: %d \n",contZero);
+    system("pause");
+}
