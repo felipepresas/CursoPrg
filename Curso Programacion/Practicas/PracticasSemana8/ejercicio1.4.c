323573a6112773c76684edba656813ba46e1e8af
felipepresas <felipe.presas@gmail.com>
2020-12-21 14:49:37 +0100

    211220201449

---
+++ Curso Programacion/Practicas/PracticasSemana8/ejercicio1.4.c
@@ -0,0 +1,23 @@
+/* Vamos a crear un programa que este pidiendo 
+numeros seguido hasta que introduzca un 0. 
+En ese momento, el programa debe terminar 
+diciendome cuantos numeros he introducido 
+y cuanto vale la suma de ellos */
+#include <stdio.h>
+int main()
+{
+    int i, num, cont = 0, sumNum;
+
+    for (i = 0; num != 0; i++)
+    {
+        printf("indique el numero");
+        scanf("%d", &num);
+        cont++;
+        sumNum += num;
+    }
+
+    printf("La cuenta de numero es :%d \n", cont);
+    printf("La sumatoria es :%d \n", sumNum);
+
+    system("pause");
+}
